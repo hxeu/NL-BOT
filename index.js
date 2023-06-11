7fa2c897fc9a6f3978f940f81e7b9e14f8635ae4
hector <hx.euwx@gmail.com>
2023-06-11 14:57:29 +0200

    trying stuff

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ import NukeCommand from './src/commands/nuke.js';
 
 config();
 
-const TOKEN = process.env.BOT_TOKEN;
-const CLIENT_ID = process.env.CLIENT_ID;
 
 const client = new Client({
   intents: [
